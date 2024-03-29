3bb8beaf711d34c61bca71552d2be27fea86423a
Amol Gore <amol_gore1@persistent.com>
2024-03-29 12:32:48 +0530

    updating the day 1 file

--- Day-1/print.py
+++ Day-1/print.py
@@ -1,2 +1,9 @@
 print('she said-"hello" and then left the chat')
 print("she said : \"hello\"and then left the hat")
+print("hello" +" "+ "amol")
+name = input("what is your name?")
+print("hello" + input("whats your name"))
+print(name)
+
+print("calculating the lenght of string")
+print(len(name))
