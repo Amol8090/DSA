926f8a77b0ab610220c93589cda87c24d6d1b0c6
Amol Gore <amol_gore1@persistent.com>
2024-04-01 11:43:08 +0530

    weeks calculation

--- Day-2/main.py
+++ Day-2/main.py
@@ -4,10 +4,16 @@
 # b=int(num[1])
 # print(a+b)
 
-print("calculatin the BMI")
-weight = int(input("enter the body weight in kg"))
-height = float(input("enter the height in meter"))
+# print("calculatin the BMI")
+# weight = int(input("enter the body weight in kg"))
+# height = float(input("enter the height in meter"))
 
-bmi=int(weight/height**2)
+# bmi=int(weight/height**2)
 
-print(f"your bmi is {bmi}")
+# print(f"your bmi is {bmi}")
+
+print("calculating the weeks left")
+age=int(input("enter the age ib years"))
+const=90
+
+print(f"you have {(const-age)*52} weeks left ")
