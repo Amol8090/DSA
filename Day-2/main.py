e0dedb1316a7d8228e306edff3d4603b9c3de54a
Amol Gore <amol_gore1@persistent.com>
2024-04-01 12:09:37 +0530

    tip calculation

--- Day-2/main.py
+++ Day-2/main.py
@@ -12,8 +12,17 @@
 
 # print(f"your bmi is {bmi}")
 
-print("calculating the weeks left")
-age=int(input("enter the age ib years"))
-const=90
+# print("calculating the weeks left")
+# age=int(input("enter the age ib years"))
+# const=90
 
-print(f"you have {(const-age)*52} weeks left ")
+# print(f"you have {(const-age)*52} weeks left ")
+
+
+print("welcome to tip calculator")
+bill= float(input("enter the total bill "))
+tip_percentage = int(input("enter the percentage of tip"))
+person = int(input("how many people are there "))
+tip = round(((bill * tip_percentage)/100),2)
+
+print(f"split of each person is {round(((bill+tip)/person),2)} ")
