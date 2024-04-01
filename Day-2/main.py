4b37c57625295cbaca39828335e702b596921765
Amol Gore <amol_gore1@persistent.com>
2024-04-01 12:10:09 +0530

    Day 2 completed

--- Day-2/main.py
+++ Day-2/main.py
@@ -1,22 +1,22 @@
-# num = "12"
+num = "12"
 
-# a=int(num[0])
-# b=int(num[1])
-# print(a+b)
+a=int(num[0])
+b=int(num[1])
+print(a+b)
 
-# print("calculatin the BMI")
-# weight = int(input("enter the body weight in kg"))
-# height = float(input("enter the height in meter"))
+print("calculatin the BMI")
+weight = int(input("enter the body weight in kg"))
+height = float(input("enter the height in meter"))
 
-# bmi=int(weight/height**2)
+bmi=int(weight/height**2)
 
-# print(f"your bmi is {bmi}")
+print(f"your bmi is {bmi}")
 
-# print("calculating the weeks left")
-# age=int(input("enter the age ib years"))
-# const=90
+print("calculating the weeks left")
+age=int(input("enter the age ib years"))
+const=90
 
-# print(f"you have {(const-age)*52} weeks left ")
+print(f"you have {(const-age)*52} weeks left ")
 
 
 print("welcome to tip calculator")
