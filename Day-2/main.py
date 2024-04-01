3289b8de1545539590bb2c44a176ca3becaab250
Amol Gore <amol_gore1@persistent.com>
2024-04-01 11:32:44 +0530

    bmi cslculation

--- Day-2/main.py
+++ Day-2/main.py
@@ -1,5 +1,13 @@
-num = "12"
+# num = "12"
 
-a=int(num[0])
-b=int(num[1])
-print(a+b)
+# a=int(num[0])
+# b=int(num[1])
+# print(a+b)
+
+print("calculatin the BMI")
+weight = int(input("enter the body weight in kg"))
+height = float(input("enter the height in meter"))
+
+bmi=int(weight/height**2)
+
+print(f"your bmi is {bmi}")
