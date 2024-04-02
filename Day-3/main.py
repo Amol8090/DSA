8059c6ea9c4c2ce113fcb46bad8b67a0d881e66a
Amol Gore <amol_gore1@persistent.com>
2024-04-02 10:13:56 +0530

    PYTHON PIZZA

--- Day-3/main.py
+++ Day-3/main.py
@@ -20,9 +20,37 @@
 #     print("you are overweight")
 
 
-year = int(input("enter the year to check"))
+# year = int(input("enter the year to check"))
 
-if (year % 4 ==0 and year % 100 !=0) or year%400==0:
-    print("its a leap year")
-else:
-    print("its not a leap year")
+# if (year % 4 ==0 and year % 100 !=0) or year%400==0:
+#     print("its a leap year")
+# else:
+#     print("its not a leap year")
+
+print("python pizza delivery")
+size=input("size of the pizza(S,M,L)")
+add_peporani = input("do you want paperoni(Y,N)")
+extra_cheese = input("do you want extra cheese(Y,N)")
+
+if size=='S':
+    bill=15
+    if add_peporani == 'Y':
+        bill+=2
+    if extra_cheese == 'Y':
+        bill+=1
+
+if size=='M':
+    bill=20
+    if add_peporani == 'Y':
+        bill+=3
+    if extra_cheese == 'Y':
+        bill+=1
+
+if size=='M':
+    bill=25
+    if add_peporani == 'Y':
+        bill+=3
+    if extra_cheese == 'Y':
+        bill+=1
+
+print(f"your bill is {bill}")
