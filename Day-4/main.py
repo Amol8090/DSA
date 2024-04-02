8a75e3a320ab995f20b744ce415da6448e555f63
Amol Gore <amol_gore1@persistent.com>
2024-04-02 18:06:53 +0530

    Day 4 ends

---
+++ Day-4/main.py
@@ -0,0 +1,9 @@
+import random
+
+demo=["rock","paper","scissor"]
+
+# choice=input("Enter your choice rock,paper,scissor").lower()
+
+comp_choice=random.choice(demo)
+print(f"computer chosse {comp_choice}")
+
