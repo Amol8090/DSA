9f6afdd0ee226802da71f00dcf295028a64c1758
Amol Gore <amol_gore1@persistent.com>
2024-04-02 11:25:00 +0530

    PYTHON PIZZA

--- Day-3/main.py
+++ Day-3/main.py
@@ -27,30 +27,46 @@
 # else:
 #     print("its not a leap year")
 
-print("python pizza delivery")
-size=input("size of the pizza(S,M,L)")
-add_peporani = input("do you want paperoni(Y,N)")
-extra_cheese = input("do you want extra cheese(Y,N)")
+# print("python pizza delivery")
+# size=input("size of the pizza(S,M,L)")
+# add_peporani = input("do you want paperoni(Y,N)")
+# extra_cheese = input("do you want extra cheese(Y,N)")
 
-if size=='S':
-    bill=15
-    if add_peporani == 'Y':
-        bill+=2
-    if extra_cheese == 'Y':
-        bill+=1
+# if size=='S':
+#     bill=15
+#     if add_peporani == 'Y':
+#         bill+=2
+#     if extra_cheese == 'Y':
+#         bill+=1
 
-if size=='M':
-    bill=20
-    if add_peporani == 'Y':
-        bill+=3
-    if extra_cheese == 'Y':
-        bill+=1
+# if size=='M':
+#     bill=20
+#     if add_peporani == 'Y':
+#         bill+=3
+#     if extra_cheese == 'Y':
+#         bill+=1
 
-if size=='M':
-    bill=25
-    if add_peporani == 'Y':
-        bill+=3
-    if extra_cheese == 'Y':
-        bill+=1
+# if size=='M':
+#     bill=25
+#     if add_peporani == 'Y':
+#         bill+=3
+#     if extra_cheese == 'Y':
+#         bill+=1
 
-print(f"your bill is {bill}")
+# print(f"your bill is {bill}")
+
+
+print("welcome to treasure finding game")
+chooice1=input('Enter where you want to go "Right" or "left".').lower()
+if chooice1=='right':
+    print("Game over!!!!!!!")
+else:
+    chooice2=input('do you want to "swim" or "wait"').lower()
+    if chooice2=='swim':
+        print("game overr!!!!")
+    else:
+        chooice3=input('enter which colour door you wnat to go "Red","Blue","Yellow"').lower()
+        if chooice3=='yellow':
+            print("you win!!!!")
+        else:
+            print("game over!!!")
