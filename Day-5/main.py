0567b71390f3bb6d299232587d8cd6c7d9bd3103
Amol Gore <amol_gore1@persistent.com>
2024-04-03 11:36:18 +0530

    range function

--- Day-5/main.py
+++ Day-5/main.py
@@ -1,12 +1,24 @@
-n=int(input("how many students"))
-lst=[]
-for i in range(0,n):
-    ele=int(input("enter height in cm "))
-    lst.append(ele)
+# n=int(input("how many students"))
+# lst=[]
+# for i in range(0,n):
+#     ele=int(input("enter height in cm "))
+#     lst.append(ele)
 
-print(lst)
-sum=0
-for height in lst:
-    sum+=height
+# print(lst)
+# sum=0
+# for height in lst:
+#     sum+=height
 
-print(f"average height of students is {sum/n}")
+# print(f"average height of students is {sum/n}")
+
+
+# highest=0
+# for ele in lst:
+#     if ele>highest:
+#         highest=ele
+    
+# print(highest)
+
+
+for i in range(2,100,2):
+    print(i)
