11ae820aeafca20a01970c3afca9d1474a76cf74
Amol Gore <amol_gore1@persistent.com>
2024-04-03 11:42:27 +0530

    FizzBuzz game

--- Day-5/main.py
+++ Day-5/main.py
@@ -20,5 +20,17 @@
 # print(highest)
 
 
-for i in range(2,100,2):
-    print(i)
+# for i in range(2,100,2):
+#     print(i)
+
+
+for i in range(0,100):
+    if i%3==0 and i%5==0:
+        print("FizzBuzz")
+    elif i%3==0:
+        print("Fizz")
+    elif i%5==0:
+        print("Buzz")
+
+    else:
+        print(i)
