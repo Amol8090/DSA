6e2acb179eb6f6aff448b66446ad905747adcd1d
Amol Gore <amol_gore1@persistent.com>
2024-04-07 20:58:23 +0530

    ceil function

--- Day-8/main.py
+++ Day-8/main.py
@@ -1,14 +1,26 @@
-###functions
+# ###functions
+import math
+# def greet():
+#     print("hello")
+#     print("how are you?")
+#     print("how was oyur day?")
 
-def greet():
-    print("hello")
-    print("how are you?")
-    print("how was oyur day?")
-
-greet()
+# greet()
 
 
-def greet_with_name(name):
-    print(f"hello {name}")
+# def greet_with_name(name):
+#     print(f"hello {name}")
 
-greet_with_name("amol")
+# greet_with_name("amol")
+
+# def greet_with(name,location):
+#     print(f"hello {name}, are you in {location}")
+
+# greet_with("amol","pune")
+
+
+def calculate(height,width):
+    return (height*width)/5
+
+paint=math.ceil(calculate(2,4))
+print(paint)
