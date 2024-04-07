872ea75b32e43ff46a46110a5fb124d5a0945ad1
Amol Gore <amol_gore1@persistent.com>
2024-04-07 20:43:51 +0530

    functions

---
+++ Day-8/main.py
@@ -0,0 +1,14 @@
+###functions
+
+def greet():
+    print("hello")
+    print("how are you?")
+    print("how was oyur day?")
+
+greet()
+
+
+def greet_with_name(name):
+    print(f"hello {name}")
+
+greet_with_name("amol")
