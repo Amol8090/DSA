d7ed179141af74b3c4aed6332ebd3bcecd8ec275
Amol Gore <amol_gore1@persistent.com>
2024-04-08 14:21:27 +0530

    Dictionary handson

--- Day-9/main.py
+++ Day-9/main.py
@@ -1,16 +1,45 @@
-programming_dictionary= {
-    "name" : "amol",
-    "age" : 21,
-    "college" : "hola university"
+# programming_dictionary= {
+#     "name" : "amol",
+#     "age" : 21,
+#     "college" : "hola university"
     
+# }
+
+# print(programming_dictionary)
+
+# programming_dictionary["company"] = "fractal"
+
+# print(programming_dictionary)
+
+# for key in programming_dictionary:
+#     print(key)
+#     print(programming_dictionary[key])
+
+
+std_marks = {
+    "amol" : 89,
+    "anuja" : 95,
+    "shubham" : 92,
+    "bhushan": 91,
+    "dummy" : 75,
+    "anup" : 31,
+    "shantaram"  : 70
 }
 
-print(programming_dictionary)
+std_grade = {
 
-programming_dictionary["company"] = "fractal"
+}
 
-print(programming_dictionary)
+for student in std_marks:
+    score = std_marks[student]
 
-for key in programming_dictionary:
-    print(key)
-    print(programming_dictionary[key])
+    if score>90:
+        std_grade[student] = "Outstanding"
+    elif score > 80:
+        std_grade[student] = "exceeds exception"
+    elif score > 70:
+        std_grade[student] = "acceptable"
+    else:
+        std_grade[student] = "fail"
+
+print(std_grade)
