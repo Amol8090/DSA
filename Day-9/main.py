6645c1bec3f439af70a9d0cc7b054b1b27d72c13
Amol Gore <amol_gore1@persistent.com>
2024-04-08 14:51:59 +0530

    nested Dictionary

--- Day-9/main.py
+++ Day-9/main.py
@@ -16,30 +16,53 @@
 #     print(programming_dictionary[key])
 
 
-std_marks = {
-    "amol" : 89,
-    "anuja" : 95,
-    "shubham" : 92,
-    "bhushan": 91,
-    "dummy" : 75,
-    "anup" : 31,
-    "shantaram"  : 70
+# std_marks = {
+#     "amol" : 89,
+#     "anuja" : 95,
+#     "shubham" : 92,
+#     "bhushan": 91,
+#     "dummy" : 75,
+#     "anup" : 31,
+#     "shantaram"  : 70
+# }
+
+# std_grade = {
+
+# }
+
+# for student in std_marks:
+#     score = std_marks[student]
+
+#     if score>90:
+#         std_grade[student] = "Outstanding"
+#     elif score > 80:
+#         std_grade[student] = "exceeds exception"
+#     elif score > 70:
+#         std_grade[student] = "acceptable"
+#     else:
+#         std_grade[student] = "fail"
+
+# print(std_grade)
+
+
+
+travelling_blog = {
+    {
+        "country" : "india",
+        "visited_city":["pune","solapur","kolhapur","nashik"],
+        "no_of city" : 10
+    },
+    {
+        "country" : "US/uk",
+        "visited_plces" : ["washigton","paris","london"],
+        "no_of_city" :9
+
+    }
+
 }
 
-std_grade = {
 
+travelling_blog = {
+    "india" : {"cities_visited" : ["pune","nashik"],"total_visites":10},
+    "goa" :{"cities visited":["panji","madgaon"],"total_visited":12}
 }
-
-for student in std_marks:
-    score = std_marks[student]
-
-    if score>90:
-        std_grade[student] = "Outstanding"
-    elif score > 80:
-        std_grade[student] = "exceeds exception"
-    elif score > 70:
-        std_grade[student] = "acceptable"
-    else:
-        std_grade[student] = "fail"
-
-print(std_grade)
