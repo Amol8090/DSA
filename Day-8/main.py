043a878f36ba89cad3a11c1aac9afe0e157f8cda
Amol Gore <amol_gore1@persistent.com>
2024-04-08 10:57:56 +0530

    day 8 completed

--- Day-8/main.py
+++ Day-8/main.py
@@ -1,22 +1,22 @@
-# ###functions
+###functions
 import math
-# def greet():
-#     print("hello")
-#     print("how are you?")
-#     print("how was oyur day?")
+def greet():
+    print("hello")
+    print("how are you?")
+    print("how was oyur day?")
 
-# greet()
+greet()
 
 
-# def greet_with_name(name):
-#     print(f"hello {name}")
+def greet_with_name(name):
+    print(f"hello {name}")
 
-# greet_with_name("amol")
+greet_with_name("amol")
 
-# def greet_with(name,location):
-#     print(f"hello {name}, are you in {location}")
+def greet_with(name,location):
+    print(f"hello {name}, are you in {location}")
 
-# greet_with("amol","pune")
+greet_with("amol","pune")
 
 
 def calculate(height,width):
@@ -24,3 +24,18 @@ def calculate(height,width):
 
 paint=math.ceil(calculate(2,4))
 print(paint)
+
+
+def prime_number(num):
+    is_prime=True
+    for i in range(2,num):
+        if num%i==0:
+            is_prime=False
+    if is_prime:
+        print("it's a prime number")
+    else:
+        print("it's not a prime number")
+
+number = 11
+
+prime_number(number)
