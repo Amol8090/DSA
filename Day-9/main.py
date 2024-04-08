dba4b32bfde69c3f3c06ea1eb0e8c975a7f3cf7d
Amol Gore <amol_gore1@persistent.com>
2024-04-08 15:36:29 +0530

    Highest bidder using Dictionary

--- Day-9/main.py
+++ Day-9/main.py
@@ -46,23 +46,58 @@
 
 
 
-travelling_blog = {
-    {
-        "country" : "india",
-        "visited_city":["pune","solapur","kolhapur","nashik"],
-        "no_of city" : 10
-    },
-    {
-        "country" : "US/uk",
-        "visited_plces" : ["washigton","paris","london"],
-        "no_of_city" :9
+# travelling_blog = [
+#     {
+#         "country" : "india",
+#         "visited_city":["pune","solapur","kolhapur","nashik"],
+#         "no_of city" : 10
+#     },
+#     {
+#         "country" : "US/uk",
+#         "visited_plces" : ["washigton","paris","london"],
+#         "no_of_city" :9
 
-    }
+#     }
+
+# ]
+
+
+# travelling_blog = {
+#     "india" : {"cities_visited" : ["pune","nashik"],"total_visites":10},
+#     "goa" :{"cities visited":["panji","madgaon"],"total_visited":12}
+# }
+
+
+import os
+
+bid_details = {
 
 }
 
+def highest_bidder(bid_details):
+    highest_bid=0
+    winner=""
+    for name in bid_details:
+        bid_value=bid_details[name]
+        if bid_value>highest_bid:
+            highest_bid=bid_value
+            winner=name
+    print(f"The winner of bidding is {winner} with the height bid of {highest_bid}")
 
-travelling_blog = {
-    "india" : {"cities_visited" : ["pune","nashik"],"total_visites":10},
-    "goa" :{"cities visited":["panji","madgaon"],"total_visited":12}
-}
+
+is_bidder=True
+
+while is_bidder:
+    name=input("enter name of bidder : ")
+    bid=int(input("enter the bid price : "))
+    bid_details[name] = bid
+    # bid_details.append(curr_bid)
+    forward=input("is there anybody left : ").lower()
+    if forward == "no":
+        is_bidder=False
+        break
+    else:
+        os.system('cls')
+        continue
+
+highest_bidder(bid_details)
