7d60aaadd67ddb7aac9b10bf2e5c580756c6ed6a
Amol Gore <amol_gore1@persistent.com>
2024-04-08 14:06:44 +0530

    Dictionary basics

---
+++ Day-9/main.py
@@ -0,0 +1,16 @@
+programming_dictionary= {
+    "name" : "amol",
+    "age" : 21,
+    "college" : "hola university"
+    
+}
+
+print(programming_dictionary)
+
+programming_dictionary["company"] = "fractal"
+
+print(programming_dictionary)
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
