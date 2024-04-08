12aaec2343edb95dffe0d3df72413678a071b137
Amol Gore <amol_gore1@persistent.com>
2024-04-08 15:37:10 +0530

    Day 9 Completed

--- Day-9/main.py
+++ Day-9/main.py
@@ -1,71 +1,71 @@
-# programming_dictionary= {
-#     "name" : "amol",
-#     "age" : 21,
-#     "college" : "hola university"
+programming_dictionary= {
+    "name" : "amol",
+    "age" : 21,
+    "college" : "hola university"
     
-# }
+}
 
-# print(programming_dictionary)
+print(programming_dictionary)
 
-# programming_dictionary["company"] = "fractal"
+programming_dictionary["company"] = "fractal"
 
-# print(programming_dictionary)
+print(programming_dictionary)
 
-# for key in programming_dictionary:
-#     print(key)
-#     print(programming_dictionary[key])
-
-
-# std_marks = {
-#     "amol" : 89,
-#     "anuja" : 95,
-#     "shubham" : 92,
-#     "bhushan": 91,
-#     "dummy" : 75,
-#     "anup" : 31,
-#     "shantaram"  : 70
-# }
-
-# std_grade = {
-
-# }
-
-# for student in std_marks:
-#     score = std_marks[student]
-
-#     if score>90:
-#         std_grade[student] = "Outstanding"
-#     elif score > 80:
-#         std_grade[student] = "exceeds exception"
-#     elif score > 70:
-#         std_grade[student] = "acceptable"
-#     else:
-#         std_grade[student] = "fail"
-
-# print(std_grade)
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
 
 
+std_marks = {
+    "amol" : 89,
+    "anuja" : 95,
+    "shubham" : 92,
+    "bhushan": 91,
+    "dummy" : 75,
+    "anup" : 31,
+    "shantaram"  : 70
+}
 
-# travelling_blog = [
-#     {
-#         "country" : "india",
-#         "visited_city":["pune","solapur","kolhapur","nashik"],
-#         "no_of city" : 10
-#     },
-#     {
-#         "country" : "US/uk",
-#         "visited_plces" : ["washigton","paris","london"],
-#         "no_of_city" :9
+std_grade = {
 
-#     }
+}
 
-# ]
+for student in std_marks:
+    score = std_marks[student]
+
+    if score>90:
+        std_grade[student] = "Outstanding"
+    elif score > 80:
+        std_grade[student] = "exceeds exception"
+    elif score > 70:
+        std_grade[student] = "acceptable"
+    else:
+        std_grade[student] = "fail"
+
+print(std_grade)
 
 
-# travelling_blog = {
-#     "india" : {"cities_visited" : ["pune","nashik"],"total_visites":10},
-#     "goa" :{"cities visited":["panji","madgaon"],"total_visited":12}
-# }
+
+travelling_blog = [
+    {
+        "country" : "india",
+        "visited_city":["pune","solapur","kolhapur","nashik"],
+        "no_of city" : 10
+    },
+    {
+        "country" : "US/uk",
+        "visited_plces" : ["washigton","paris","london"],
+        "no_of_city" :9
+
+    }
+
+]
+
+
+travelling_blog = {
+    "india" : {"cities_visited" : ["pune","nashik"],"total_visites":10},
+    "goa" :{"cities visited":["panji","madgaon"],"total_visited":12}
+}
 
 
 import os
